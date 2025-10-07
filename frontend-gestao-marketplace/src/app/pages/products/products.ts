10cca74411ac0018d4c355331d9332cceedd32e5
Bruno Sérvio <bruno.servio@somosicev.com>
2025-10-07 16:28:57 -0300

    show products page

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products';
+import { IProductResponse } from '../../interfaces/product-response';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   imports: [],
   templateUrl: './products.html',
-  styleUrl: './products.css'
+  styleUrl: './products.css',
 })
-export class Products {
+export class Products implements OnInit {
+  products: IProductResponse[] = [];
+  private readonly _productsService = inject(ProductsService);
 
+  ngOnInit() {
+    this._productsService
+      .getProducts()
+      .pipe(take(1))
+      .subscribe({
+        next: (response) => {
+          this.products = response.data;
+        },
+      });
+  }
 }
